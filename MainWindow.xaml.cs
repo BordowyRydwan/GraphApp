c24cc20a3d40d2ae8af959df2b3ef25fa1ccfac0
Dawid Wijata <s205006@sggw.edu.pl>
2021-06-14 14:04:48 +0200

    Rearrange UI, delete graphical mode

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,57 +25,20 @@ namespace GraphApp
 
     public partial class MainWindow : Window
     {
-        bool isTextMode = false;
-        bool isGraphicalMode = true;
-
-        SolidColorBrush activeButtonColor = RGBConverter.RGBStringToColorBrush("#159072");
-        SolidColorBrush toPressButtonColor = RGBConverter.RGBStringToColorBrush("#5E807F");
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new GraphicalViewModel();
-        }
-
-        private void TextModeToggle_Click(object sender, RoutedEventArgs e)
-        {
-            if (isTextMode)
-            {
-                return;
-            }
-
-            isTextMode = true;
-            isGraphicalMode = false;
-
-            GraphicsModeToggle.Background = toPressButtonColor;
-            TextModeToggle.Background = activeButtonColor;
-
             DataContext = new TextViewModel();
         }
 
-        private void GraphicsModeToggle_Click(object sender, RoutedEventArgs e)
+        private void TextButton_Click(object sender, RoutedEventArgs e)
         {
-            if (isGraphicalMode)
-            {
-                return;
-            }
-
-            isTextMode = false;
-            isGraphicalMode = true;
-
-            GraphicsModeToggle.Background = activeButtonColor;
-            TextModeToggle.Background = toPressButtonColor;
-
-            DataContext = new GraphicalViewModel();
+            DataContext = new TextViewModel();
         }
+
 
         private void HelpButton_Click(object sender, RoutedEventArgs e)
         {
-            isTextMode = false;
-            isGraphicalMode = false;
-
-            GraphicsModeToggle.Background = toPressButtonColor;
-            TextModeToggle.Background = toPressButtonColor;
-
             DataContext = new HelpViewModel();
         }
     }
